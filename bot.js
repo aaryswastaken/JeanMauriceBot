fa8e2212d22df789ea2e4c36bcc126de4a822bda
Vincent SAHLER <vincent.sahler@gmail.com>
2021-08-31 13:23:44 +0200

    Procedure commands implementation

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const fs = require("fs");
-const {Client, Intents} = require("discord.js");
+const {Client, Intents, DMChannel} = require("discord.js");
 const client = new Client({intents: [
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MEMBERS,
@@ -20,6 +20,23 @@ const client = new Client({intents: [
     ]});
 
 const discriminator = "!";
+let cache = {};
+
+
+///// CACHE MANAGEMENT
+function refreshCache() {
+    fs.writeFileSync("./cache.json", JSON.stringify(cache))
+}
+
+function readCache() {
+    cache = JSON.parse(fs.readFileSync("./cache.json"));
+
+    if(!Object.keys(cache).includes("activeComm")) {cache.activeComm = {}}
+}
+
+readCache();
+
+///// CONFIG
 
 let config = JSON.parse(fs.readFileSync("./config.json"));
 
@@ -32,41 +49,148 @@ function format(str) {
     return s
 }
 
-function handleNewUser(member) {
-    console.log(`Handling new user ... ${member.user.username}`);
-
-    member.user.send(format(config.welcomeMessage))
+function createDM(user, then) {
+    user.createDM().then((channel) => {
+        cache.activeComm[user.id].DMChannelID = channel.id;
+        refreshCache();
+        then(channel);
+    })
 }
 
-function commandHandler(msg, message) {
+function procedure(user) { // This function will handle the communication with the user
+    switch (cache.activeComm[user.id].state) {
+
+    }
+}
+
+function beginProcedure(user) { // This function handles the start of the procedure with clean user state
+    cache.activeComm[user.id].state = 0;
+
+    refreshCache();
+
+    procedure(user);
+}
+
+function handleNewUser(user, force) { // This function is triggered whenever someones joins the server or type !newuser
+    console.log(`Handling new user ... ${user.username}`);
+
+    if(!Object.keys(cache.activeComm).includes(user.id) || force) {
+        cache.activeComm[user.id] = {};
+
+        createDM(user, (channel) => {
+            config.messages.welcomeMessages.forEach(m => {
+                channel.send(format(m));
+            });
+
+            cache.activeComm[user.id].state = 0;
+
+            refreshCache();
+
+            beginProcedure(user);
+        })
+    } else {
+        client.channels.fetch(cache.activeComm[user.id].DMChannelID).send(format(config.messages.alreadyProcessing))
+    }
+}
+
+function handleAbortUser(user) { // !abort
+    if(Object.keys(cache.activeComm).includes(user.id)) {
+        client.channels.fetch(cache.activeComm[user.id].DMChannelID)
+            .then((DMChannel) => {
+                DMChannel.send(format(config.messages.abortMessage));
+                cache.activeComm[user.id].state = -1;
+
+                refreshCache();
+            });
+    } else {
+        createDM(user, (channel) => {
+            channel.send(format(config.messages.abortNotStarted))
+        })
+    }
+}
+
+function handleRestartUser(user) { // !restart
+    if(Object.keys(cache.activeComm).includes(user.id)) {
+        client.channels.fetch(cache.activeComm[user.id].DMChannelID)
+            .then((DMChannel) => {
+                DMChannel.send(format(config.message.restartMessage));
+            });
+
+        cache.activeComm[user.id].state = 0;
+
+        refreshCache();
+
+        beginProcedure(user);
+    } else {
+        handleNewUser(user, true);
+    }
+}
+
+function commandHandler(msg, message, isMP) { // Handles every commands (starting with "!")
     console.log(`New command ${msg}`)
     let ct = msg.split(" ");
     switch (ct[0].substring(discriminator.length)) {
         case "newuser":
-            handleNewUser(message.member);
+            handleNewUser(message.author, false);
+            break;
+        case "abort":
+            handleAbortUser(message.author);
+            break;
+        case "restart":
+            handleRestartUser(message.author);
             break;
     }
 }
 
-client.on("guildMemberAdd", (member) => {
+client.on("guildMemberAdd", (member) => { // Whenever someone joins the server
     handleNewUser(member)
 })
 
-client.on("messageCreate", (message) => {
-    /* console.log(`${message.author.username} : ${message.content}`)
+client.on("messageCreate", (message) => { // Whenever someone send a message
+    console.log(`New message from ${message.author.username} on ${message.guildId} : ${message.content}`);
     if(!message.author.bot) {
-        message.reply("Truc")
-            .then(() => console.log(`Replied to message "${message.content}"`))
-            .catch(console.error);
-    } */
-    if(!message.author.bot) {
-        let msg = message.content.toLowerCase().replace(/ */, "");
+        let msg = message.content.toLowerCase().replace(/ */, ""); // Deletes every spaces before the message
         if(msg[0] === discriminator) {
-            commandHandler(msg, message);
+            commandHandler(msg, message, (message.guildId === null));
         }
     }
 })
 
-client.login(process.env.TOKEN)
-    .then(r => console.log("JEAN MAURICE IS UP !"))
-    .catch(console.error)
+client.login(process.env.TOKEN) // Entrypoint 
+    .then(r => {
+        console.log("[*] Restoring DM ... ");
+        if(Object.keys(cache.activeComm).length > 0) {
+            for (let [id, value] of Object.entries(cache.activeComm)) {
+                console.log(`Restoring ... #${value.DMChannelID}`);
+                client.channels.fetch(value.DMChannelID);
+            }
+        }
+
+        console.log("JEAN MAURICE IS UP !");
+    })
+    .catch(console.error);
+
+function exitHandler(options, exitCode) { // Whenever the bot stops
+    if (options.cleanup) {
+        console.log('----- STOPPING -----');
+
+        refreshCache(); console.log("[!] Cache saved")
+    }
+    if (exitCode || exitCode === 0) console.log(exitCode);
+    if (options.exit) process.exit();
+}
+
+// REGISTER STOPPING PROCEDURE
+
+//do something when app is closing
+process.on('exit', exitHandler.bind(null,{cleanup:true}));
+
+//catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+
+// catches "kill pid" (for example: nodemon restart)
+process.on('SIGUSR1', exitHandler.bind(null, {exit:true}));
+process.on('SIGUSR2', exitHandler.bind(null, {exit:true}));
+
+//catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
